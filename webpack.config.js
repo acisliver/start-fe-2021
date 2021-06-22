93b66b122c32e3c8e0c61fa25c2c3fdf517d0ac7
acisliver <7273322@naver.com>
2021-06-22 15:16:33 +0900

    웹팩 개발 환경 세팅

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'app.js',
+        path: `${__dirname}/dist`,
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
