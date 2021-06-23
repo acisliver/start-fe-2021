a5e6593da3955fd4daaa78b067588e89c932b528
acisliver <7273322@naver.com>
2021-06-23 23:47:25 +0900

    json 데이터 입력

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    //엔트리 파일의 위치 1개의 번들
     entry: './src/app.js',
+    //빌드되어 배포될 위치
     output: {
         filename: 'app.js',
         path: `${__dirname}/dist`,
     },
+    //Auto Reloading을 위한 위치
     devServer: {
         contentBase: './dist',
     },
